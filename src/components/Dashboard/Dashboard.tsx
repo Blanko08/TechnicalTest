30414b04b7a0428c433fbdf0c513b6c8cd856184
Santiago Herrera Blanco <santiago@glue.digital>
2023-05-18 23:23:37 +0200

    Fix bug when metamask is not installed

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -54,7 +54,7 @@ const Dashboard = () => {
         }
     }
 
-    window.ethereum.on('chainChanged', () => { if (!isCorrectNetwork) handleNetworkChange() })
+    window.ethereum?.on('chainChanged', () => { if (!isCorrectNetwork) handleNetworkChange() })
 
     useEffect(() => {
       (async () => {
